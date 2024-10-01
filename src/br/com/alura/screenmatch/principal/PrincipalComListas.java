2f9e9c8bdc601418ea5375f5a46bc77912a65f68
Bruno Henrique Adriano Rehm <bruno.rehm.3@gmail.com>
2024-10-01 18:44:03 -0300

    Ordenação por ano de lançamento

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -7,16 +7,20 @@ import br.com.alura.screenmatch.modelos.Titulo;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
         Filme filme1 = new Filme("O poderoso chefão", 1970);
         filme1.avalia(9);
+        filme1.avalia(9);
 
         Filme filme2 = new Filme("Avatar", 2023);
         filme2.avalia(6);
+        filme2.avalia(6);
 
         Filme filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
         filmeDoPaulo.avalia(10);
 
         Serie lost = new Serie("Lost", 2000);
@@ -35,6 +39,7 @@ public class PrincipalComListas {
             if (item instanceof Filme) {
                 System.out.println("Classificação: " + ((Filme) item).getClassificacao());
             }
+            System.out.println();
         }
 
         ArrayList<String> buscaPorArtistas = new ArrayList<>();
@@ -45,12 +50,20 @@ public class PrincipalComListas {
         buscaPorArtistas.add("Paulo");
         buscaPorArtistas.add("Jaqueline");
         System.out.println(buscaPorArtistas);
+        System.out.println();
 
         Collections.sort(buscaPorArtistas);
         System.out.println("Depois da ordenação");
         System.out.println(buscaPorArtistas);
+        System.out.println();
 
+        System.out.println("Ordenação por nome");
         Collections.sort(lista);
+        System.out.println(lista);
+        System.out.println();
+
+        System.out.println("Ordenação por ano de lançamento");
+        lista.sort(Comparator.comparing(Titulo::getAnoLancamento));
         System.out.println(lista);
     }
 }
